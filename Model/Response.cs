29d35d5709d4f0d5c89252de5aa2739f9d1ded8f
Michelle Mayambu <43130998+MichelleMayambu@users.noreply.github.com>
2021-04-14 22:28:05 +0200

    Update Response.cs

--- Model/Response.cs
+++ Model/Response.cs
@@ -11,17 +11,5 @@ namespace AssesmentApi.Model
         public string msg { get; set; }
         public dynamic data { get; set; }
     }
-    public class DataResponse
-    {
-        public long ID { get; set; }
-        public string PayChannelRef { get; set; }
-        public string PaymentMessageTrace { get; set; }
-        public string UserID { get; set; }
-        public string MeterNumber { get; set; }
-        public decimal Amount { get; set; }
-        public string ResponseStatus { get; set; }
-        public string PaySeqID { get; set; }
-        public Guid PaymentTransactionRef { get; set; }
-
-    }
+  
 }
